6911c9830f52f24a5f4ac1a989a09592ad76e197
joaonery1 <joao.nery1@hotmail.com>
2021-04-12 21:10:53 -0300

    some changes

--- teste.py
+++ teste.py
@@ -7,7 +7,7 @@ b = "[]"
 lista = []
 final = ''
 with open('interpreterworkflow.wk') as arquivo:
-    dados = arquivo.readlines()[57:62]  # array de strings
+    dados = arquivo.readlines()[57:78]  # array de strings
     # print(dados)
     texto = dados  # varíavel para armazenar o array de string
     for letra in texto:  # for para transformar em string
@@ -16,14 +16,17 @@ with open('interpreterworkflow.wk') as arquivo:
         
         resp = filtro.findall(letra)  # funcao para trasnformar em números
         #resp = list(map(int, resp))  # função apra transformar em inteiros
-        print(resp)
+        #print(resp)
 
        # print(resp)
-        for ares in resp:
+        for ares in resp:                  #for para percorrer nas linhas de 'numeros'
             resp = list(map(int, resp))
-            lista.append(ares)
-#print(lista)       
-
+            lista.append(ares)             #add dos valores em uma lista
+print(tuple(lista))                        #transforma a lista em tuplas
+def chunks(lista, n):
+    for i in range(0, len(lista), n): # fiz um for para fazer as dívisoes de listas, para melhor visualizacao
+        yield lista[i:i + n]
+print(list(chunks(lista, 2)))   #divide em sublsitas de 2 
 
 '''
     filtro =  re.compile('([0-9]+)')
