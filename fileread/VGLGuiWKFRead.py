28535a857d1a2905869dec5565975112c8b13a67
mrobertowagner <mrobertowagner@yahoo.com.br>
2021-05-30 09:44:49 -0300

    Criação estrutura do Glifo na memória

--- fileread/VGLGuiWKFRead.py
+++ fileread/VGLGuiWKFRead.py
@@ -1,24 +1,41 @@
 # Objective: read VGLGui workflow file and load content into memory
 # File type: VGLGuiTemplate.wkf
+
+# Glyph object structure
+from collections import defaultdict
+
+class objGlyph(object):
+        
+    def __init__(self, vlibrary, vlocalhost, vglyph_id, vglyph_name, vglyph_x, vglyph_y, vlst_par):
+        
+        self.library = vlibrary       #library name (Ex: VisionGL)
+        self.localhost = vlocalhost   #folder where the image file or library function is located
+        self.glyph_id = vglyph_id     #glyph identifier code
+        self.glyph_name = vglyph_name #VisionGL image or function name
+        self.glyph_x = vglyph_x       #numerical coordinate of the glyph's linear position on the screen 
+        self.glyph_y = vglyph_y       #numerical coordinate of the column position of the Glyph on the screen
+        self.lst_par = vlst_par       #parameter list
+
+
 #
 import os
 
 # File to be read
-def vfile = str
+vfile = str
 vfile = "VGLGui.wkf"
 
 # Structure to store data in memory
 
 # Glyph - graphical representation of an image or function from the VisionGL library
-def vlibrary = ''    #library name (Ex: VisionGL)
-def vlocalhost = ''  #folder where the image file or library function is located
-def vglyph_id = 0    #glyph identifier code
-def vglyph_name = '' #VisionGL image or function name
-def vglyph_x = 0     #numerical coordinate of the glyph's linear position on the screen 
-def vglyph_y = 0     #numerical coordinate of the column position of the Glyph on the screen
-def vlst_par = []    #parameter list
+vlibrary = ''    #library name (Ex: VisionGL)
+vlocalhost = ''  #folder where the image file or library function is located
+vglyph_id = 0    #glyph identifier code
+vglyph_name = '' #VisionGL image or function name
+vglyph_x = 0     #numerical coordinate of the glyph's linear position on the screen 
+vglyph_y = 0     #numerical coordinate of the column position of the Glyph on the screen
+vlst_par = []    #parameter list
 
-
+lstGlyph 
 
 # NodeConnection - graphic representation of the connection between two glyphs
 def output_glyph_id = 0 # glyph identifier code output
