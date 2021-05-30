b0b345bdc24bac3bc0eb932e12befcf51f1a3d8d
mrobertowagner <mrobertowagner@yahoo.com.br>
2021-05-30 16:48:11 -0300

    v2

--- fileread/VGLGuiWKFRead.py
+++ fileread/VGLGuiWKFRead.py
@@ -35,6 +35,7 @@ vlst_par = []    #parameter list
 
 # List to store glyphs
 lstGlyph = []
+conteudo = []
 
 # NodeConnection - graphic representation of the connection between two glyphs
 #def output_glyph_id = 0 # glyph identifier code output
@@ -42,16 +43,18 @@ lstGlyph = []
 
 #
 def fileRead(lstGlyph):
-    if os.path.isfile(vfile):
-        file1 = open(vfile,"r")
+    if os.path.isfile("VGLGui.wkf"):
+        file1 = open("VGLGui.wkf","r")
         for line in file1:
-            if 'Glyph' in line.lower():
+            if 'glyph' in line.lower():
                 conteudo = line.split(':')
                 for column in conteudo:
-                    lstGlyph.append(conteudo)
+                    lstGlyph.append(conteudo[conteudo])
         file1.close()
 
 # Program execution
 lstGlyph = []
+conteudo = []
+
 fileRead(lstGlyph)
 print(lstGlyph)
