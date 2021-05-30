281294523351f734c0b445343bf665ffeefb92ce
joaonery1 <joao.nery1@hotmail.com>
2021-05-30 18:12:19 -0300

    testes de algumas implementações

--- mk.py
+++ mk.py
@@ -3,7 +3,7 @@
 def __init__(self):
   self.add_vertex()
 
-def add_vertex(*v):
+def add_vertex(v):
   global graph
   global vertices_no
   if v in graph:
@@ -48,7 +48,7 @@ desvio_padrao:7:o:i:11
 
 graph = {}
 vertices_no = 0
-add_vertex(1, 3, 5, 7, 9)
+
 add_vertex(1)
 add_vertex(3)
 add_vertex(5)
@@ -68,3 +68,49 @@ output
 {1: [[1, 'i', 3, 'o']], 3: [[3, 'i', 5, 'o'], [3, 'i1', 7, 'o1']], 
 5: [[5, 'i', 9, 'o']], 7: [[7, 'i', 11, 'o']], 9: [], 11: []}
 '''
+
+#funcao para armazenar os parametros
+def fun_node(var):
+    var = var.split(':')
+    var = var[2::]
+    #print(var)
+    lista_entrada = []
+    lista_saida = []
+    lista_entrada.append(var[3])
+    lista_saida.append(var[1])
+    return lista_entrada,lista_saida
+
+#funcao para armazenar as arestas
+def fun_duplas(parametros):
+    lista = []
+    filtro = re.compile('([0-9]+)')
+    resp = filtro.findall(parametros)
+    for duplas in resp:
+        resp = list(map(int,resp))
+        lista.append(duplas)   
+    arestas = list(map(int,lista))
+    return arestas
+
+    
+#funcao a ser trabalhada para armazenar as informacoes dos glifos
+def fun_gliph (var):
+    var = var.split(':')
+    #talvez usar o regex
+    novos_dados = ''
+    novos_dados+= '{};{};{};{};{};{};{};{};{};{}\n'.format(var[:4],var[4:7],var[7:11],var[11:13],var[13:15],var[15:17],
+    var[17:18],var[18:25],var[25:26],var[26:32],var[32:33])
+    content = '{}\n{}'.format(';'.join(var),novos_dados)
+
+
+    #funcao para pegar os vertices dos glifos
+def fun_vertex(vertex): 
+    vertex = vertex.split(':')
+    vertex = vertex[3]
+    valores = []
+    lista_vertices = []
+    valores.append(vertex)
+    for i in valores: #for para passar uma lista de inteiros
+        lista_vertices.append(int(i))
+    return lista_vertices
+
+
