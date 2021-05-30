4314ada3a7b9e382a5554f8eda5c769797bf936d
mrobertowagner <mrobertowagner@yahoo.com.br>
2021-05-30 19:54:16 -0300

    Inclusão armazenamento das conexões na memória

--- fileread/VGLGuiWKFRead.py
+++ fileread/VGLGuiWKFRead.py
@@ -5,11 +5,9 @@
 import re
 import os
 import string
-
-# Glyph object structure
 from collections import defaultdict
 
-# Structure to store data in memory
+# Structure for storing Glyphs in memory
 class objGlyph(object):
         
     def __init__(self, vlibrary, vlocalhost, vglyph_id, vglyph_name, vglyph_x, vglyph_y, vlst_par):       
@@ -21,40 +19,58 @@ class objGlyph(object):
         self.glyph_y = vglyph_y       #numerical coordinate of the column position of the Glyph on the screen
         self.lst_par = vlst_par       #parameter list
 
+# Structure for storing Connections in memory
+class objConnection(object):
+        
+    def __init__(self, voutput_glyph_id, vinput_glyph_id):       
+        self.output_glyph_id = voutput_glyph_id    #glyph identifier code output
+        self.input_glyph_id = vinput_glyph_id      #glyph identifier code input
+
+
 # File to be read
-vfile = 'VGLGui.wkf'
+vfile = 'fileread/VglGui.wkf'
 
-# Glyph - graphical representation of an image or function from the VisionGL library
-vlibrary = ''    #library name (Ex: VisionGL)
-vlocalhost = ''  #folder where the image file or library function is located
-vglyph_id = 0    #glyph identifier code
-vglyph_name = '' #VisionGL image or function name
-vglyph_x = 0     #numerical coordinate of the glyph's linear position on the screen 
-vglyph_y = 0     #numerical coordinate of the column position of the Glyph on the screen
-vlst_par = []    #parameter list
+lstGlyph = []                #List to store Glyphs
+lstConnection = []           #List to store Connections
+vGlyph = objGlyph            #Glyph in memory 
+vConnection = objConnection  #Connection in memory
 
-# List to store glyphs
-lstGlyph = []
-conteudo = []
-
-# NodeConnection - graphic representation of the connection between two glyphs
-#def output_glyph_id = 0 # glyph identifier code output
-#def input_glyph_id = 0  # glyph identifier code input
-
-#
+# Method for reading the workflow file
 def fileRead(lstGlyph):
-    if os.path.isfile("fileread/vglgui.wkf"):
-        file1 = open("fileread/vglgui.wkf","r")
+    if os.path.isfile(vfile):
+
+        # Opens the workflow file
+        file1 = open(vfile,"r")
         for line in file1:
-            if 'glyph' in line.lower():
-                content = line.split(':')
-                for column in content:
-                    lstGlyph.append(content[conteudo])
+
+            # Creates the glyphs of the workflow file
+            if 'glyph:' in line.lower():
+                contentGly = line.split(':')
+                vGlyph = objGlyph(contentGly[1], contentGly[2], contentGly[3], contentGly[4], contentGly[5], contentGly[6], contentGly[8])
+                lstGlyph.append(vGlyph)
+
+            # Creates the connections of the workflow file
+            if 'NodeConnection:' in line.lower():
+                contentCon = line.split(':')
+                vConnection = objConnection(contentCon[1], contentCon[2])
+                lstConnection.append(vConnection)
+
         file1.close()
 
 # Program execution
 lstGlyph = []
-content = []
+lstConnection = []
+contentGly = []
+contentCon = []
 
+# Reading the workflow file
 fileRead(lstGlyph)
-print(lstGlyph)
+
+# Shows the content of the Glyphs
+for vGlyph in lstGlyph:
+    print("Library:", vGlyph.library, "Localhost:", vGlyph.localhost, "Glyph_Id:", vGlyph.glyph_id, "Glyph_Name:", vGlyph.glyph_name,
+          "Position_Line:", vGlyph.glyph_x, "Position_Column:", vGlyph.glyph_y, "Parameters:", vGlyph.lst_par)
+
+# Shows the content of the Connections
+for vConnection in lstConnection:
+    print("Glyph_Output_Id:", vConnection.output_glyph_id, "Glyph_Input_Id:", vConnection.input_glyph_id)
