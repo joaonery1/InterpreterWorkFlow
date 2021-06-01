eb0dc8e18497bee455a2251045d92c0325462906
joaonery1 <joao.nery1@hotmail.com>
2021-06-01 02:22:40 -0300

    modificações

--- funwork.py
+++ funwork.py
@@ -6,11 +6,48 @@ import string
 # -*- coding: utf-8 -* 
 from collections import defaultdict
 
+class objGlyph(object):
+    def __init__(self, vglyph_id,):       
+        self.glyph_id = vglyph_id    
+    
+    def __repr__(self):
+        return "{}".format(self.glyph_id)
+
+
+
+class objConnection(object):
+
+    def __init__(self,u_id,u_output,v_id,v_input):
+
+        self.u_id= u_id
+        self.u_output = u_output
+        self.v_id = v_id
+        self.v_input = v_input
+    
+    def __repr__(self):
+        return "{},{}".format(self.u_output,self.v_input)
+
+    def chunks(lista,n):
+        for i in range(0,len(lista),n):
+            yield lista[i:i +n]
+        return list(lista)
+
+lstConnection = []
+vConnection = objConnection
+vGlyph = objGlyph
+
 #funcao para fazer sublistas dado um N
 def chunks(lista,n):
     for i in range(0,len(lista),n):
         yield lista[i:i +n]
    
+#funcao teste para str - > int
+def int_str(lst):
+    lst = []
+    lista_n = []
+    for valores in lst:
+        lista_n.append(int(valores))
+    return lista_n
 
 def vertex_graph(lstv):
     if os.path.isfile("arquivo2.wk"):
@@ -37,55 +74,62 @@ def node_connection(lstu):
 
 
 #ideia de leitura com parametros
-def node_connection_parameters(lstu,lstv,listin,listout,listassc):
+def connection_parameters(lstGlyph):
     if os.path.isfile("interpreterworkflow.wk"):
         file1 = open("interpreterworkflow.wk","r")
+        
         for line in file1:
+            if 'glyph:' in line.lower():
+                conexao = line.split(':')
+                vGlyph = objGlyph(conexao[3])
+                lsti.append(vGlyph)
+
             if 'nodeconnection' in line.lower():
                 conexao = line.split(':')
-                lstu.append(conexao[1])
-                lstu.append(conexao[3])
-                lstv.append(conexao[3])
-                listin.append(conexao[4])
-                listout.append(conexao[2])
-                listassc.append(conexao[1])
-                listassc.append(conexao[2])
-                listassc.append(conexao[3])
-                listassc.append(conexao[4])
-                #pensar em outra forma de aramazenar nessa lista os parametros associados
-                #dessa forma pode dizer que ele está associado?
-                #pensar em criar uma funcao apenas para ler parametros? Ou funcao para ler os dois?
-                #verificar um jeito de tirar o '/n' no final de caracter no .split 
-                #talvez criar uma função para transformar uma lista de str em int, ao inves de fazer um for na 'raiz'
+                vConnection = objConnection(conexao[1],conexao[2],conexao[3],conexao[4])
+                lstConnection.append(vConnection)
         file1.close()
 
+lstGlyph = []
+lstConnection = []
+lstedge = []
+lstparam = []
+lstedges = []
+lsti = []
+lstvertex = []
+connection_parameters(lstGlyph)
+for vConnection in lstConnection:
+    lstedge.append(vConnection.u_id)
+    lstedge.append(vConnection.v_id)
+    lstparam.append(vConnection.u_output)
+    lstparam.append(vConnection.v_input)
+    #print(vConnection.u_id,vConnection.v_id)
+
+for vGlyph in lstGlyph:
+    lsti.append(vGlyph.vglyph_id)
+
+
+for val in lstedge:
+    lstedges.append(int(val))
+
+print(lsti)
+lstedges = list(chunks(lstedges,2))
+lstcomplete= [*sum(zip(lstedges,lstparam),())]    #juntei depois de alimentar a lista
+lstcomplete = list(chunks(lstcomplete,4))    
+
+print(lsti)   #lista de vertices 
+print(lstedges) #lista de arestas
+print(lstcomplete) #lista arestas c/ parametros
+
+
+############################################################
+
 #script para leitura do arquivo e aplicao das funcoes
-
 lstarestas= []
-
 lstv = []
 listout= []
 listin = []
 listassc = []
-'''
-with open('interpreterworkflow.wk') as file:
-    for line in file:
-        if 'nodeconnection' in line.lower():
-            # entradas = fun_node(line) #recebe as duas listas de parametros
-            # arestas = fun_duplas(line)   #recebe as arestas 
-            conexao = line.split(':')
-            lstu.append(conexao[2],)
-            lstu.append(conexao[4])
-            lstout.append(conexao[3])
-            lstout.append(conexao[5])
-
-            
-        elif 'glyph' in line.lower():
-            fun_gliph(line)       
-            vertices = fun_vertex(line) #recebe a lista de vertices
-            #funcao para tratar os glyphos e suas variaveis
-'''
-#print(lstu,lstout)
 
 vertex_graph(lstv)
 node_connection(lstarestas)
@@ -98,15 +142,6 @@ for val in lstv:
     lista_vertices.append(int(val))
 lista_arestas = list(chunks(lista_arestas,2)) #nova lista tratada
 lstarestas = list(chunks(lstarestas,2))       #nova lsita tratada
-#print(lista_arestas)
-#print(lista_vertices)
-
-#aplicação da função
-lstu1 = []
-node_connection_parameters(lstu1,lstv,listin,listout,listassc)
-
-
-
 
 #função para criar grafo
 def cria_grafo(lista_de_vertices, lista_de_arestas):
@@ -116,7 +151,7 @@ def cria_grafo(lista_de_vertices, lista_de_arestas):
     for aresta in lista_de_arestas:
         grafo[aresta[0]].append(aresta[1])
         grafo[aresta[1]].append(aresta[0])
-    return grafo
+    return grafo    
 
 #função que percorre o grafo inicio->fim
 def dfs_caminhos(grafo, inicio, fim):
@@ -131,21 +166,27 @@ def dfs_caminhos(grafo, inicio, fim):
 
 '''
 lista genérica
-l_de_vertices = [1,2,3,4,5,6,7,8,9,10]
-l_de_arestas = [[1, 2], [1, 6], [1, 8], [3, 6], [3, 10], [5, 2], [5, 4], [7, 4], [7, 8], [9, 2], [9, 4]]
+
 '''
-grafo = cria_grafo(lista_vertices,lista_arestas)
+l_de_vertices = [1, 2, 3 ,4, 5, 6, 7, 8, 9, 10]
+l_de_arestas = [[1, 2], [1, 6], [1, 8], [3, 6], [3, 10], [5, 2], [5, 4], [7, 4], [7, 8], [9, 2], [9, 4]]
+
+grafo = cria_grafo(l_de_vertices,l_de_arestas)
 
 print("Representação\n",grafo)
 print("####################################")
-caminhos = list(dfs_caminhos(grafo,1,9))
-print("Caminhos percorridos\n",caminhos)
+#caminhos = list(dfs_caminhos(grafo,1,9))
+#print("Caminhos percorridos\n",caminhos)
 print("####################################")
 
 #separa em sublistas
-lista_assc = list(chunks(listassc,4))
-print(lista_assc)
+#lista_assc = list(chunks(listassc,4))
+lista_asso = []
+
+
+
+
 
 '''
-implementar para um grafo junto com os parametros
+verificar erro ao ao criar o grafo com a lista de objtos
 '''
