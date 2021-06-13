310d68436774b05411662d030d4938e78d637f2b
joaonery1 <joao.nery1@hotmail.com>
2021-06-13 15:58:41 -0300

    novas modificações

--- copyGuiWk.py
+++ copyGuiWk.py
@@ -352,4 +352,3 @@ for vGlyph in lstGlyph:
 for vConnection in lstConnection:
     print("Conexão:", vConnection.type, "Glyph_Output_Id:", vConnection.output_glyph_id, "Glyph_Output_Varname:", vConnection.output_varname,
           "Glyph_Input_Id:", vConnection.input_glyph_id, "Glyph_Input_Varname:", vConnection.input_varname)
-#teste
