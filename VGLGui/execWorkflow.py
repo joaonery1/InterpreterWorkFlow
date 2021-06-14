786e9d1d072f16281011ba4561a7ae23ab04b13a
joaonery1 <joao.nery1@hotmail.com>
2021-06-14 12:15:10 -0300

    V1

--- VGLGui/execWorkflow.py
+++ VGLGui/execWorkflow.py
@@ -1,8 +1,13 @@
-#from readWorkflow import lstGlyph, lstConnection, lstGlyphIn, lstGlyphOut
-#
-#    for vGlyph in lstGlyph:
-#        if vGlyph.getGlyphReady():
-#            for vConnection in lstConnection:
-#                for vGlyphIn in lstGlyphIn:
-#                    for vGlyphOut in lstGlyphOut:
-#                        print('oi')
+#from .readWorkflow import lstConnection
+import os, sys , inspect
+
+sys.path.append(os.getcwd())
+from VGLGui.readWorkflow import lstGlyph,lstConnection,lstGlyphIn,lstGlyphOut
+
+
+for vGlyph in lstGlyph:
+         if vGlyph.getGlyphReady():
+             for vConnection in lstConnection:
+                 for vGlyphIn in lstGlyphIn:
+                     for vGlyphOut in lstGlyphOut:
+                         print('oi')
