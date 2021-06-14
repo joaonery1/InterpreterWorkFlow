ac43cc0491e39e4121a42fbba420737d1513d006
mrobertowagner <mrobertowagner@yahoo.com.br>
2021-06-14 03:42:12 -0300

    v0 programa execução leitura workflow

---
+++ VGLGui/execWorkflow.py
@@ -0,0 +1,8 @@
+from readWorkflow import lstGlyph, lstConnection, lstGlyphIn, lstGlyphOut
+
+        for vGlyph in lstGlyph:
+            if vGlyph.getGlyphReady():
+                for vConnection in lstConnection:
+                    for vGlyphIn in lstGlyphIn:
+                        for vGlyphOut in lstGlyphOut:
+                            print('oi')
