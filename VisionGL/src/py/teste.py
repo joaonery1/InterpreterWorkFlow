96b069aa090aace9879f14b05664c52e99fda310
joaonery1 <joao.nery1@hotmail.com>
2021-06-22 21:23:00 -0300

    v1

---
+++ VisionGL/src/py/teste.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# OPENCL LIBRARY
+import pyopencl as cl
+
+# VGL LIBRARYS
+import vgl_lib as vl
+
+# TO WORK WITH MAIN
+import numpy as np
+
+# IMPORTING METHODS
+from cl2py_ND import * 
+
+import time as t
+import sys
+
+
+glifo = input('Digite o nome do glifo ')
+
+vl.vglClInit()
+
+msg = ""
+
+	# INPUT IMAGE
+img_input = vl.VglImage(sys.argv[1], None, vl.VGL_IMAGE_2D_IMAGE(), vl.IMAGE_ND_ARRAY())
+vl.vglLoadImage(img_input)
+vl.vglClUpload(img_input)
+
+	# OUTPUT IMAGE
+img_output = vl.create_blank_image_as(img_input)
+img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
+vl.vglAddContext(img_output, vl.VGL_CL_CONTEXT())
+
+# STRUCTURANT ELEMENT
+window = vl.VglStrEl()
+window.constructorFromTypeNdim(vl.VGL_STREL_CROSS(), 2)
+
+if glifo == 'Copy':
+    inicio = t.time()
+    vglClNdCopy(img_input, img_output)
+    fim = t.time()
+    vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
+    vl.vglSaveImage("img-vglNdCopy.jpg", img_output)
+    msg = msg + "Tempo de execução do método vglClNdCopy:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"
+else:
+    print("ERROR")
