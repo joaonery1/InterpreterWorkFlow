8959ff0a600272ce6553889099b9fe2a71b91313
joaonery1 <joao.nery1@hotmail.com>
2021-07-02 00:28:22 -0300

    execução

--- VisionGL/src/py/work.py
+++ VisionGL/src/py/work.py
@@ -1,54 +1,29 @@
-from readWorkflow import *
-
-# IMPORTING METHODS FROM VISIONGL
-#sys.path.insert(0,'VisionGL/src/py')
-#from VisionGL.src.py import benchmark_clnd
-
-import os
-os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1'
-
-#import pyopencl as cl
+#!/usr/bin/env python3
+# OPENCL LIBRARY
+import pyopencl as cl
 
 # VGL LIBRARYS
-#import vglImage as vl
+import vgl_lib as vl
 
 # TO WORK WITH MAIN
 import numpy as np
 
 # IMPORTING METHODS
-#from cl2py_ND import * 
+from cl2py_shaders import * 
+
+
 
 # IMPORTING METHODS FROM VGLGui
 import sys
+import os
+
+sys.path.append(os.getcwd())
+sys.path.insert(0,'/VGLGui/')
+from VGLGui.readWorkflow import *
+
 
 import time as t
-
-#vl.vglClInit()     #inicio das estruturas vgl
-
-#Show info
-def procShowInfo():
-    for vGlyph in lstGlyph:
-        print("Library:", vGlyph.library, "Function:", vGlyph.func, "Localhost:", vGlyph.localhost, "Glyph_Id:", vGlyph.glyph_id, 
-            "Position_Line:", vGlyph.glyph_x, "Position_Column:", vGlyph.glyph_y)#, "Parameters:", vGlyph.lst_par)
-
-        #Shows the list of glyph inputs
-        for vGlyphIn in vGlyph.lst_input:
-            print("Glyph_Id:", vGlyph.glyph_id, "Glyph_In:", vGlyphIn)
-
-        #Shows the list of glyph outputs
-        for vGlyphOut in vGlyph.lst_output:
-            print("Glyph_Id:", vGlyph.glyph_id, "Glyph_Out:", vGlyphOut)
-
-    # Shows the content of the Connections
-    for vConnection in lstConnection:
-        print("Conexão:", vConnection.type, "Glyph_Output_Id:", vConnection.output_glyph_id, "Glyph_Output_Varname:", vConnection.output_varname,
-            "Glyph_Input_Id:", vConnection.input_glyph_id, "Glyph_Input_Varname:", vConnection.input_varname)
-
-# Program execution
-
-# Reading the workflow file and loads into memory all glyphs and connections
-fileRead(lstGlyph)
-
+fileRead(lstGlyph) #leitura do arquivo
 def salvando2d(img, name):
 	# SAVING IMAGE img
 	ext = name.split(".")
@@ -63,8 +38,6 @@ def salvando2d(img, name):
 	
 	vl.vglSaveImage(name, img)
 
-
-
 img_in_path = sys.argv[1]
 nSteps		= int(sys.argv[2])
 img_out_path= sys.argv[3]
@@ -73,70 +46,108 @@ msg = ""
 
 vl.vglClInit()
 
+img_input = vl.VglImage(img_in_path, None, vl.VGL_IMAGE_2D_IMAGE())
+vl.vglLoadImage(img_input)
+if( img_input.getVglShape().getNChannels() == 3 ):
+    vl.rgb_to_rgba(img_input)
 
+vl.vglClUpload(img_input)
 
-#Update the status of glyph entries
+img_output = vl.create_blank_image_as(img_input)
+img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
+vl.vglAddContext(img_output, vl.VGL_CL_CONTEXT())
+
+convolution_window_2d_3x3 = np.array((	(1/16, 2/16, 1/16),
+										(2/16, 4/16, 2/16),
+										(1/16, 2/16, 1/16) ), np.float32)
+convolution_window_2d_5x5 = np.array((	(1/256, 4/256,  6/256,  4/256,  1/256),
+										(4/256, 16/256, 24/256, 16/256, 4/256),
+										(6/256, 24/256, 36/256, 24/256, 6/256),
+										(4/256, 16/256, 24/256, 16/256, 4/256),
+										(1/256, 4/256,  6/256,  4/256,  1/256) ), np.float32)
+'''
+vglClBlurSq3(img_input, img_output)
+media = 0.0
+for i in range(0, 5):
+	p = 0
+	inicio = t.time()
+	while(p < nSteps):
+		vglClBlurSq3(img_input, img_output)
+		p = p + 1
+		fim = t.time()
+		media = media + (fim-inicio)
+salvando2d(img_output, img_out_path+"img-vglClBlurSq3.jpg")
+vl.rgb_to_rgba(img_output)
+msg = msg + "Tempo de execução do método vglClBlurSq3:\t\t" +str( round( ( media / 5 ), 9 ) ) +"s\n"
+
+vglClCopy(img_input, img_output)
+media = 0.0
+for i in range(0, 5):
+    p = 0
+    inicio = t.time()
+    while(p < nSteps):
+        vglClCopy(img_input, img_output)
+        p = p + 1
+    fim = t.time()
+    media = media + (fim-inicio)
+
+salvando2d(img_output, img_out_path+"img-vglClCopy.jpg")
+vl.rgb_to_rgba(img_output)
+msg = msg + "Tempo de execução do método vglClCopy:\t\t\t" +str( round( (media / 5), 9 ) ) +"s\n"
+
+vglClThreshold(img_input, img_output, np.float32(0.5))
+media = 0.0
+for i in range(0, 5):
+    p = 0
+    inicio = t.time()
+    while(p < nSteps):
+        vglClThreshold(img_input, img_output, np.float32(0.5))
+        p = p + 1
+    fim = t.time()
+    media = media + (fim-inicio)
+
+salvando2d(img_output, img_out_path+"img-vglClThreshold.jpg")
+vl.rgb_to_rgba(img_output)
+msg = msg + "Tempo de execução do método vglClThreshold:\t\t" +str( round( (media / 5), 9 ) ) +"s"
+'''
+
 for vGlyph in lstGlyph:
 
-    #Glyph execute
-    if vGlyph.getGlyphReady() and vGlyph.getGlyphDone() == False:
-        #Image Input
-        if vGlyph.func == 'in':
-            img_input = vl.VglImage(img_in_path, None, vl.VGL_IMAGE_2D_IMAGE())
-            vl.vglLoadImage(img_input)
-            if( img_input.getVglShape().getNChannels() == 3 ):
-                vl.rgb_to_rgba(img_input)
-            
-            vl.vglClUpload(img_input)
-            
-            img_output = vl.create_blank_image_as(img_input)
-            img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
-            vl.vglAddContext(img_output, vl.VGL_CL_CONTEXT())
-            
-            convolution_window_2d_3x3 = np.array((	(1/16, 2/16, 1/16),
-                                                    (2/16, 4/16, 2/16),
-                                                    (1/16, 2/16, 1/16) ), np.float32)
-            convolution_window_2d_5x5 = np.array((	(1/256, 4/256,  6/256,  4/256,  1/256),
-                                                    (4/256, 16/256, 24/256, 16/256, 4/256),
-                                                    (6/256, 24/256, 36/256, 24/256, 6/256),
-                                                    (4/256, 16/256, 24/256, 16/256, 4/256),
-                                                    (1/256, 4/256,  6/256,  4/256,  1/256) ), np.float32)
+    if vGlyph.func == 'in': #imagem de entrada
+        print("")
+    
+    elif vGlyph.func == 'vstrflat': #elementro estruturante 
+        print("")
+    
+    elif vGlyph.func == 'out': #imagem de saida
+        print("")
 
-        #elif vGlyph.func == 'vstrflat': #Structuring element 
-        #    window = vl.VglStrEl()
-        #    window.constructorFromTypeNdim(vl.VGL_STREL_CROSS(), 2)
+    elif vGlyph.func == 'kmul': #funcao copy
+        vglClCopy(img_input, img_output)
+        media = 0.0
+        for i in range(0, 5):
+            p = 0
+            inicio = t.time()
+            while(p < nSteps):
+                vglClCopy(img_input, img_output)
+                p = p + 1
+        fim = t.time()
+        media = media + (fim-inicio)
 
-        #elif vGlyph.func == 'out': #Image Output
-        #    img_output = vl.create_blank_image_as(img_input)
-        #    img_output.set_oclPtr( vl.get_similar_oclPtr_object(img_input) )
-        #    vl.vglAddContext(img_output, vl.VGL_CL_CONTEXT())
+        salvando2d(img_output, img_out_path+"img-vglClCopy.jpg")
+        vl.rgb_to_rgba(img_output)
+        msg = msg + "Tempo de execução do método vglClCopy:\t\t\t" +str( round( (media / 5), 9 ) ) +"s\n"
+        #print(msg)
 
-        #elif vGlyph.func == 'kmul': #Function copy
-        #    inicio = t.time()
-        #    vglClNdCopy(img_input, img_output)
-        #    fim = t.time()
-        #    vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-        #    vl.vglSaveImage("img-vglNdCopy.jpg", img_output)
-        #    msg = msg + "Tempo de execução do método vglClNdCopy:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"
-        #    print(msg)
+    elif vGlyph.func == 'vero': #funcao erosion
+        print("")
 
-        #elif vGlyph.func == 'vero': #Function erosion
-        #    inicio = t.time()
-        #    vglClNdDilate(img_input, img_output, window)
-        #    fim = t.time()
-        #    vl.vglCheckContext(img_output, vl.VGL_RAM_CONTEXT())
-        #    vl.vglSaveImage("img-vglNdDilate.jpg", img_output)
-        #    msg = msg + "Tempo de execução do método vglClNdDilate:\t" +str( round( (fim-inicio), 9 ) ) +"s\n"
-        #    print(msg)
+print("-------------------------------------------------------------")
+print(msg)
+print("-------------------------------------------------------------")
 
-        #Sets all glyph outputs as executed 
-        vGlyph.setGlyphOutputAll(True)
+img_input = None
+img_output = None
 
-        #Sets all glyph outputs as executed
-        vGlyph.setGlyphInputAll(True)
-
-        #Defines that the glyph was executed
-        vGlyph.setGlyphDone(True)
-
-# Shows the content of the Glyphs
-procShowInfo()
+convolution_window_2d_5x5 = None
+convolution_window_2d_3x3 = None	
